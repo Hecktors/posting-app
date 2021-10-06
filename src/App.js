d59250c2fc37c4b9a5050a0de48b2993a01e6b3d
Torsten Heckmann <torstenheckmann@hectorair.localdomain>
2021-10-07 00:22:41 +0200

    Add navbar and user logout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {useState} from 'react';
 import Login from './components/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   const [user, setUser] = useState(null)
 
   return (
     <div className="App">
+      <Navbar setUser={setUser} isAuth={!!user}/>
       { !user?.id && <Login setUser={setUser}/> }
     </div>
   );
