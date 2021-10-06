697dfa642f382f69af2ca4d69cc058a6cadefbe9
Torsten Heckmann <torstenheckmann@hectorair.localdomain>
2021-10-07 00:11:03 +0200

    Add login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {useState} from 'react';
+import Login from './components/Login';
 
 function App() {
+  const [user, setUser] = useState(null)
 
   return (
     <div className="App">
-     
+      { !user?.id && <Login setUser={setUser}/> }
     </div>
   );
 }
---
+++ src/api/userAPI.js
@@ -0,0 +1,14 @@
+export const checkLogin = (loginData) => {
+    return fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then(users => {
+        const foundUser = users.find(user => user.email === loginData.email)
+        if(!foundUser || foundUser.username !== loginData.password) {return false};
+        return {
+            id: foundUser.id,
+            name: foundUser.name,
+            username: foundUser.username,
+            email: foundUser.email
+        }
+    })
+}
